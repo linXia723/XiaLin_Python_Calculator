1b8ca44973b167bc26b68d5d30571bf83bf305c9
Fareeda030 <19101967@qq.com>
2026-03-29 20:37:21 +0800

    Add the subtraction operation

--- Python_Calculator.py
+++ Python_Calculator.py
@@ -1,1 +1,7 @@
+##USing the function to subtract two numbers
 
+def subtract():
+	a=float(input("Enter the first number:"))
+	b=float(input("Enter the second number:"))
+
+	print(f"The result of the subtraction is ",a-b)
