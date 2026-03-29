08db4bed05bf0be18f74348e99bd493b00112a7e
Fareeda030 <19101967@qq.com>
2026-03-29 20:54:28 +0800

    Change the function to a more concise one

--- Python_Calculator.py
+++ Python_Calculator.py
@@ -1,7 +1,4 @@
 ##USing the function to subtract two numbers
 
-def subtract():
-	a=float(input("Enter the first number:"))
-	b=float(input("Enter the second number:"))
-
-	print(f"The result of the subtraction is ",a-b)
+def subtract(a,b):
+	print("The result of the subtraction is ",a-b)
