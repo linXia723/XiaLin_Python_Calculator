014bc6bcbfaaa664596a2c000cd32e9735bb637d
linXia723 <18014911082@163.com>
2026-03-29 21:26:31 +0800

    Fix indentation in divide function

--- Python_Calculator.py
+++ Python_Calculator.py
@@ -3,7 +3,7 @@ def add(x,y):
 
 def divide():
 	a=float(input("Enter the first number:"))
-  b=float(input("Enter the second number:"))
+ 	b=float(input("Enter the second number:"))
 
 	print(f"The result of the subtraction is ",a/b)
 
